9d6ca5d2508ae984084df8f2216a884e0ca052f1
lerushka <vderkachova@gmail.com>
2015-03-19 20:56:36 +0200

    вывод на экран введенных пользователем слов

--- app-valerka/Program.cs
+++ app-valerka/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace app_valerka
 {
+    //написать программу которая:
+    // принимает на вход строку
+    // выводит эти слова в случайном порядке
     class Program
     {
+        static string GetInput()
+        {
+            string str;
+            str = Console.ReadLine();
+            return str;
+        }
         static void Main(string[] args)
         {
+            string input = GetInput();
+            Console.WriteLine(input);
+
+            //задержка
+            Console.ReadKey();
         }
     }
 }
